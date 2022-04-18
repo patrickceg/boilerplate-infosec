c834ca035f9471d3240256ad026ce58f1ea409db
Patrick S <patrickceg@outlook.com>
2022-04-17 20:15:12 -0400

    Fix copy-paste error

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const helmet = require("helmet");
 const app = express();
 app.use(helmet.hidePoweredBy());
 // Don't allow me to be in ANY frames:
-app.use(frameguard({ action: "deny" }));
+app.use(helmet.frameguard({ action: "deny" }));
 
 
 
